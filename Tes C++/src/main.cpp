e5a7a52f74303e21e2546933f94347cc3607b8db
Axel Thoriq Anggara <axelta46@gmail.com>
2024-09-12 09:25:10 +0700

    Update main.cpp

--- Tes C++/src/main.cpp
+++ Tes C++/src/main.cpp
@@ -1,9 +1,57 @@
+// #include "Meja.h"
+// #include "Pelanggan.h"
+// #include "Pelayan.h"
+// #include "Dapur.h"
+// #include <thread>
+// #include <chrono>
+
+// // Simulate decreasing customer emotion over time
+// void emosiPelangganTurun(Pelanggan& pelanggan) {
+//     while (pelanggan.getEmosi() > 1) {
+//         std::this_thread::sleep_for(std::chrono::seconds(2)); // Emotion decreases every 2 seconds
+//         pelanggan.kurangiEmosi();
+//         pelanggan.tampilkanEmosi();
+//     }
+// }
+
+// int main() {
+//     Meja meja1;
+//     Pelanggan pelanggan1;
+//     Pelayan pelayan1;
+//     Dapur dapur1;
+
+//     // Start a thread to simulate customer's emotion decreasing over time
+//     std::thread threadEmosi(emosiPelangganTurun, std::ref(pelanggan1));
+
+//     // Menampilkan status awal pesanan di meja
+//     meja1.tampilkanStatusPesanan();
+
+//     // Pelayan mengambil pesanan dan mengirim ke dapur
+//     pelayan1.ambilPesanan(meja1);
+//     pelayan1.kirimPesananKeDapur();
+//     meja1.tampilkanStatusPesanan();
+
+//     // Dapur memulai memasak pesanan
+//     dapur1.mulaiMemasak();
+//     dapur1.selesaiPesanan(meja1);
+
+//     // Pelayan mengantarkan pesanan ke meja
+//     pelayan1.antarPesanan(meja1, pelanggan1);
+//     meja1.tampilkanStatusPesanan();
+
+//     // Wait for the emotion thread to finish
+//     threadEmosi.join();
+
+//     return 0;
+// }
 #include "Meja.h"
 #include "Pelanggan.h"
 #include "Pelayan.h"
 #include "Dapur.h"
 #include <thread>
 #include <chrono>
+#include <iostream>
+#include <windows.h> // For Windows key handling
 
 // Simulate decreasing customer emotion over time
 void emosiPelangganTurun(Pelanggan& pelanggan) {
@@ -23,21 +71,42 @@ int main() {
     // Start a thread to simulate customer's emotion decreasing over time
     std::thread threadEmosi(emosiPelangganTurun, std::ref(pelanggan1));
 
-    // Menampilkan status awal pesanan di meja
-    meja1.tampilkanStatusPesanan();
+    bool gameRunning = true;
+    bool orderTaken = false;
+    bool orderReady = false;
 
-    // Pelayan mengambil pesanan dan mengirim ke dapur
-    pelayan1.ambilPesanan(meja1);
-    pelayan1.kirimPesananKeDapur();
-    meja1.tampilkanStatusPesanan();
+    while (gameRunning) {
+        // Check for 'F' key to take order
+        if (GetAsyncKeyState('F') & 0x8000) { // If the 'F' key is pressed
+            if (!orderTaken) {
+                pelayan1.ambilPesanan(meja1);
+                pelayan1.kirimPesananKeDapur();
+                meja1.tampilkanStatusPesanan();
+                dapur1.mulaiMemasak();  // Simulate cooking
+                dapur1.selesaiPesanan(meja1);
+                orderTaken = true;
+                orderReady = true;
+            }
+        }
 
-    // Dapur memulai memasak pesanan
-    dapur1.mulaiMemasak();
-    dapur1.selesaiPesanan(meja1);
+        // Check for 'D' key to deliver order
+        if (GetAsyncKeyState('D') & 0x8000) { // If the 'D' key is pressed
+            if (orderReady) {
+                pelayan1.antarPesanan(meja1, pelanggan1);
+                meja1.tampilkanStatusPesanan();
+                orderReady = false;
+                orderTaken = false;
+            }
+        }
 
-    // Pelayan mengantarkan pesanan ke meja
-    pelayan1.antarPesanan(meja1, pelanggan1);
-    meja1.tampilkanStatusPesanan();
+        // Exit the game loop with 'Esc' key
+        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) { // If 'Esc' key is pressed
+            gameRunning = false;
+        }
+
+        // Sleep for a short while to avoid excessive CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     // Wait for the emotion thread to finish
     threadEmosi.join();
