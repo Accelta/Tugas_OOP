9c3a28a8ca5a528a899f72d64e3e8ec6334f368e
Axel Thoriq Anggara <axelta46@gmail.com>
2024-09-12 09:31:45 +0700

    Update main.cpp

--- Tes C++/src/main.cpp
+++ Tes C++/src/main.cpp
@@ -1,49 +1,3 @@
-// #include "Meja.h"
-// #include "Pelanggan.h"
-// #include "Pelayan.h"
-// #include "Dapur.h"
-// #include <thread>
-// #include <chrono>
-
-// // Simulate decreasing customer emotion over time
-// void emosiPelangganTurun(Pelanggan& pelanggan) {
-//     while (pelanggan.getEmosi() > 1) {
-//         std::this_thread::sleep_for(std::chrono::seconds(2)); // Emotion decreases every 2 seconds
-//         pelanggan.kurangiEmosi();
-//         pelanggan.tampilkanEmosi();
-//     }
-// }
-
-// int main() {
-//     Meja meja1;
-//     Pelanggan pelanggan1;
-//     Pelayan pelayan1;
-//     Dapur dapur1;
-
-//     // Start a thread to simulate customer's emotion decreasing over time
-//     std::thread threadEmosi(emosiPelangganTurun, std::ref(pelanggan1));
-
-//     // Menampilkan status awal pesanan di meja
-//     meja1.tampilkanStatusPesanan();
-
-//     // Pelayan mengambil pesanan dan mengirim ke dapur
-//     pelayan1.ambilPesanan(meja1);
-//     pelayan1.kirimPesananKeDapur();
-//     meja1.tampilkanStatusPesanan();
-
-//     // Dapur memulai memasak pesanan
-//     dapur1.mulaiMemasak();
-//     dapur1.selesaiPesanan(meja1);
-
-//     // Pelayan mengantarkan pesanan ke meja
-//     pelayan1.antarPesanan(meja1, pelanggan1);
-//     meja1.tampilkanStatusPesanan();
-
-//     // Wait for the emotion thread to finish
-//     threadEmosi.join();
-
-//     return 0;
-// }
 #include "Meja.h"
 #include "Pelanggan.h"
 #include "Pelayan.h"
@@ -60,6 +14,13 @@ void emosiPelangganTurun(Pelanggan& pelanggan) {
         pelanggan.kurangiEmosi();
         pelanggan.tampilkanEmosi();
     }
+}
+
+// Function to reset the table and customer for the next round
+void pelangganBaru(Meja& meja, Pelanggan& pelanggan) {
+    std::cout << "\nPelanggan baru datang!" << std::endl;
+    meja.ubahStatusPesanan("Belum Diambil");  // Reset table status
+    pelanggan.resetEmosi();                   // Reset customer emotion
 }
 
 int main() {
@@ -96,6 +57,9 @@ int main() {
                 meja1.tampilkanStatusPesanan();
                 orderReady = false;
                 orderTaken = false;
+
+                // Introduce a new customer after the order is delivered
+                pelangganBaru(meja1, pelanggan1);
             }
         }
 
